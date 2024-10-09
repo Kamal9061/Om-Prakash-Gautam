c225306f35bfd9960106fd9212e8014b61e38747
Kamal Jeet Arya <96496716+Kamal9061@users.noreply.github.com>
2024-10-09 18:22:40 +0530

    first commit

---
+++ src/Schemas/Data.js
@@ -0,0 +1,149 @@
+export const Data = {
+  profile: {
+    name: "Om Prakash Gautam",
+    ocupation: "Sr. Talent Acquisition Specialist",
+    location: "Mumbai, Maharashtra, India",
+    email: "omprakashgautam221@gmail.com",
+    telephone: "+91 75065 12411",
+    image: "images/ompra.jpeg",
+  },
+  aboutMe: {
+    label: "Profile",
+    description:
+    "Experienced IT Recruiter with over 2 years and 11 months of expertise in sourcing and placing top-tier IT professionals across various technical domains. Proven track record in recruitment for roles including Java, .NET, Oracle SCM, SAP ABAP, AWS Cloud Engineer, Android Developer, ReactJS, React Native, SAP MM, SAP QA, PLSQL, Automation Testing, Manual Testing, IT Sales, Business Development, and Procurement. Strong ability to assess technical proficiency, cultural fit, and match candidates with client requirements effectively."
+  },
+  skills: {
+    technicalLabel: "Key Skills",
+    softLabel: "Certifications",
+    technicalSkills: [
+      "Technical Recruitment",
+      "Candidate Assessment and Screening",
+      "Client Relationship Management",
+      "Strategic Hiring Plans",
+      "Sourcing and Talent Acquisition",
+      "Recruitment Strategy Development",
+      "Interviewing Techniques",
+      "Candidate Engagement and Experience",
+    ],
+    softSkills: [
+      "Naukri Maestro Recruiter Certification",
+      "Recruiting Foundations",
+      "Technical Recruiting Learning",
+      "LinkedIn Recruiter",
+      "Performance-Based Hiring"
+    ],
+  },
+  socialMedia: {
+    label: "SOCIAL",
+    social: [
+      {
+        label: "Visit my LinkedIn",
+        name: "linkedin",
+        url: "https://www.linkedin.com/in/om-prakash-gautam-0877b81b7",
+        className: "bxl-linkedin-square",
+      },
+    ],
+  },
+  experience: {
+    works: [
+      {
+        title: "Sr. Talent Acquisition Specialist",
+        period: "May 2024 - Present",
+        company: "Mangrow HR India Pvt Ltd",
+        description: [
+          "Develop and implement strategic recruitment plans tailored to IT industry needs, aligning with business goals.",
+          "Source high-caliber talent through diverse channels, including job boards, social media, and professional networks.",
+          "Conduct in-depth assessments of candidates' technical skills and cultural fit to ensure alignment with client requirements.",
+          "Collaborate with clients and hiring managers to understand and fulfill their talent requirements.",
+          "Coordinate and manage client relationships, maintaining clear communication and ensuring client satisfaction throughout the recruitment process.",
+          "Optimize and streamline recruitment processes using best practices and innovative strategies for improved efficiency and effectiveness.",
+          "Ensure compliance with industry standards and regulatory requirements, upholding high levels of professionalism and integrity.",
+          "Utilize data-driven insights and analytics to drive recruitment strategies, measure success, and identify areas for improvement",
+          "Provide regular updates and feedback to clients and internal teams."
+        ],
+      },
+      {
+        title: "Talent Acquisition Executive",
+        period: "July 2022 - April 2024",
+        company: "Teckraft Infosolutions Pvt Ltd",
+        description: [
+          "Led end-to-end recruitment processes, from sourcing and screening to interviewing and onboarding, for junior to senior IT positions.",
+"Developed and executed strategic hiring plans to address IT staffing needs across various technology domains.",
+"Built and maintained strong client relationships, effectively managing IT recruitment needs in a dynamic and competitive market.",
+"Organized and led successful campus recruitment drives, identifying and nurturing emerging talent and contributing to a strong talent pipeline.",
+"Utilized exceptional communication skills to engage with candidates, present job opportunities, and facilitate smooth interactions between candidates and clients.",
+"Mentored and guided recruitment consultants, leading teams to deliver high-quality candidates and maintain a robust talent pool.",
+"Managed vendor relationships to ensure performance met or exceeded client expectations and negotiated terms as needed.",
+ "Implemented social media advertising strategies on platforms like LinkedIn, Naukri, and Indeed to attract top talent and increase visibility."
+        ],
+      },
+      {
+        title: "Human Resources Executive Recruitment",
+        period: "November 2021 - July 2022",
+        company: "Career Chef (Placements) India Pvt. Ltd.",
+        description: [
+          "Conducted strategic sourcing of candidates from various job portals and professional networks to build a strong talent pipeline.",
+"Screened resumes and conducted initial candidate assessments to evaluate technical skills, communication proficiency, and overall suitability for roles.",
+"Coordinated and managed interview processes, liaising between candidates and partner companies to facilitate effective evaluations and hiring decisions.",
+"Demonstrated exceptional communication skills to articulate job opportunities, build rapport with candidates, and enhance their overall experience.",
+"Conducted industry and market research to align recruitment strategies with organizational goals and evolving market demands.",
+"Cultivated positive relationships with candidates, providing guidance and support throughout the recruitment process and contributing to successful career transitions"
+        ],
+      },
+      {
+        title: "Administrative Assistant",
+        period: "September 2017 - November 2021",
+        company: "EUROPA BIOCARE PRIVATE LIMITED",
+        description: [
+"Supported daily administrative operations to enhance overall organizational efficiency.",
+"Coordinated with marketing and sales teams to streamline communication and facilitate smooth operations.",
+"Organized and documented marketing research, providing valuable insights for departmental review and strategy development.",
+"Assisted the sales team with customer account management, documentation, and support for accurate and timely processing.",
+"Prepared detailed reports based on competitive product analysis, contributing to strategic decision-making.",
+" Managed inventory records, reconciled customer account balances, and resolved discrepancies efficiently.",
+"Scheduled and organized meetings, events, and appointments, ensuring effective time management and coordination.",
+"Conducted market research, analyzed data, and reported findings to support business development initiatives.",
+"Handled administrative tasks including email correspondence, office supply management, and front desk support.",       
+        ],
+      },
+    ],
+    academic: [
+      {
+        career: "Bachelor of Commerce (B.Com) in Accounting",
+        date: "June 2021 - Awaiting Results",
+        institution: "Dr. Ram Manohar Lohia Awadh University, Faizabad",
+      },
+      {
+        career: "Intermediate in Accounting",
+        date: "July 2016 - April 2018",
+        institution: "Yaduvanshi Sen Sec School, Faizabad, Uttar Pradesh",
+      },
+    ],
+    Projects: [
+      {
+        name: "Hospital Management System",
+        company: "",
+        period: "",
+        description: [
+          "A progressive web app for both doctors and patients. Medico, where people can get monitored by the doctor and consult for better health",
+        ],
+      },
+      {
+        name: "Quiz Application",
+        company: "",
+        period: "",
+        description: [
+          "Developed a website where a user play quiz questions and get score. Tech stack- React Js , Java Script , Git and MongoDB.",
+        ],
+      },
+      {
+        name: "Restorant App",
+        company: "",
+        period: "",
+        description: [
+          "Developed features like login, signup, sending emails, payment gateway, post feeds & chatbox using websocket. Tech stack- Nodejs & MongoDB",
+        ],
+      }
+    ],
+  },
+};
